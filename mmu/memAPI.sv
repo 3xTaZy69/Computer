cf50d5bfa9977981521df31e0025178f04e6d570
extazy <nazargudakov@google.com>
2026-07-13 19:04:07 +0300

    Basic memory interface

--- mmu/memAPI.sv
+++ mmu/memAPI.sv
@@ -1,36 +1,61 @@
+/*
+
+    July 13th, 2026
+
+    Temporary simulation-only memory controller
+
+*/
+
 module memAPI (
-    input logic clk, write,
+    input logic clk, write, load,
     input logic [2:0] f3,
     input logic [31:0] wdata, addr,
     output logic [31:0] rdata 
 );
-    /*
-    logic wdata8, addr8, rdata8, mclk;
-    logic [2:0] state;
 
-    memc m (
-        .clk (mclk),
-        .write (write),
-        .wdata (wdata8),
-        .addr (addr8),
-        .rdata (rdata8)
-    );
-
-    initial begin
-        wdata8 = 0;
-        addr8 = 0;
-        rdata8 = 0;
-        state = 0;
-        mclk = 0;
-    end
-
+    logic [7:0] mem [4095:0];
 
     always_ff @(posedge clk) begin
-        if (state == 0) begin
-            
+        if (write) begin
+            case (f3)
+                3'b000: begin
+                    mem[addr] <= wdata[7:0];
+                end
+                3'b001: begin
+                    mem[addr] <= wdata[7:0];
+                    mem[addr+1] <= wdata[15:8];
+                end
+                3'b010: begin
+                    mem[addr] <= wdata[7:0];
+                    mem[addr+1] <= wdata[15:8];
+                    mem[addr+2] <= wdata[23:16];
+                    mem[addr+3] <= wdata[31:24];
+                end
+            endcase
+        end 
+
+        if (load) begin
+            case (f3)
+                3'b000: begin
+                    logic [7:0] data;
+                    data = mem[addr];
+                    rdata <= {{24{data[7]}}, data};
+                end
+                3'b001: begin
+                    logic [15:0] data;
+                    data = {mem[addr+1], mem[addr]};
+                    rdata <= {{16{data[15]}}, data};
+                end
+                3'b010: begin
+                    rdata <= {mem[addr+3], mem[addr+2], mem[addr+1], mem[addr]};
+                end
+                3'b100: begin
+                    rdata <= {24'b0, mem[addr]};
+                end
+                3'b101: begin
+                    rdata <= {16'b0, mem[addr+1], mem[addr]};
+                end
+            endcase
         end
     end
-    */
-
-    
 endmodule
